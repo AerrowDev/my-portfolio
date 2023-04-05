d939c99166fc3686eaa325a71c4b3ee3644fa822
AerrowDev <sgaeprellegera@gmail.com>
2023-04-05 22:03:42 +0800

    fix about page and nav active links

--- src/Components/about-section2.js
+++ src/Components/about-section2.js
@@ -6,37 +6,38 @@ import Imagenav from '/src/Components/logo-nav'
 const Aboutsection2 = () =>{
     return (
       
-        <section class="about section" id="about">
+    <section class="about section" id="about">
         <h2 class="section__title">About Me </h2>
         <span class="section__subtitle">My Introduction</span>
 
         <div class="about__container container grid">
-     
+            <img src="assets/img/2x2 23 - final.jpg" alt="" class="about__img"/>
             <div class="about__data">
                 <p class="about__description">
-                    I am a web developer and graphic designer with a passion for creating visually appealing and
-                    functional websites and designs. My skills include HTML, CSS, JavaScript, and Adobe Creative
-                    Suite. I bring projects from concept to completion with my technical and creative abilities.
+                I am an experienced web developer and graphic designer with a passion for creating visually appealing and functional websites and designs. 
+                I have expertise in HTML, CSS, JavaScript, and the Adobe Creative Suite, and have expanded my skills to include several popular frameworks such as Laravel, Next.js, and React. 
+                I have successfully completed various projects using these frameworks and I bring projects from concept to completion with my technical and creative abilities.
+                 My goal is to deliver high-quality work that meets the needs of clients and exceeds their expectations.
                 </p>
                 <div class="about__info">
                     <div>
-                        <span class="about__info-title">1</span>
-                        <span class="about__info-name">Year&apos;  experience</span>
+                        <span class="about__info-title">01+</span>
+                        <span class="about__info-name">Year/s <br/> experience</span>
                     </div>
 
                     <div>
-                        <span class="about__info-title">15</span>
-                        <span class="about__info-name">Completed  Project</span>
+                        <span class="about__info-title">5+</span>
+                        <span class="about__info-name">Completed <br/> Project</span>
                     </div>
 
                     <div>
-                        <span class="about__info-title">2</span>
-                        <span class="about__info-name">Companies worked</span>
+                        <span class="about__info-title">02+</span>
+                        <span class="about__info-name">Companies <br/> worked</span>
                     </div>
                 </div>
 
                 <div class="about__buttons">
-                    <a download="" href="assets/pdf/Aaron-Cv (2).pdf" class="button button--flex">
+                    <a download="" href="assets/pdf/AaronPrellegera-CV.pdf" class="button button--flex">
                         Download CV <i class="uil uil-download-alt button__icon"></i>
                     </a>
                 </div>
@@ -44,6 +45,7 @@ const Aboutsection2 = () =>{
         </div>
 
     </section>
+
 
     );
 }
--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,66 +1,63 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import {navMenu} from '/src/utilities/header'
-const Header = () =>{
-    return (
-        <header className="header" id="header">
-        <nav className="nav container">
-        <Link href="https://www.linkedin.com/in/aaron-prellegera-5ba681211" target="_blank"
-                            class="home__social-icon">
-                            <i class="uil uil-linkedin-alt "></i>
-                        </Link>
+import styles from '@/styles/Home.module.css'
+import { useRouter } from 'next/router';
 
-                        <Link href="https://web.facebook.com/AerrowDevPH" target="_blank" class="home__social-icon">
-                          <i class="uil uil-facebook-f"></i>
-                        </Link>
+function Header() {
+  const router = useRouter();
 
-                        <Link href="https://github.com/AerrowDev" target="_blank" class="home__social-icon">
-                            <i class="uil uil-github-alt"></i>
-                        </Link>
-            <div className="nav__menu" id="nav-menu">
-                <ul className="nav__list grid">
-                    <li className="nav__item">
-                        <Link href="/" className="nav__link active-link text-decoration-none">
-                            <i className="uil uil-estate nav__icon "></i>Home
-                        </Link>
-                    </li>
-                    <li className="nav__item">
-                        <Link href='/about' className="nav__link text-decoration-none">
-                            <i className="uil uil-user nav__icon "></i>About
-                        </Link>
-                    </li>
-                    <li className="nav__item">
-                        <Link href="#skills" className="nav__link text-decoration-none">
-                            <i className="uil uil-file-alt nav__icon "></i>Skills
-                        </Link>
-                    </li>
-                    <li className="nav__item">
-                        <Link href="#services" className="nav__link text-decoration-none">
-                            <i className="uil uil-briefcase-alt nav__icon "></i>Services
-                        </Link>
-                    </li>
-                    <li className="nav__item">
-                        <Link href="#portfolio" className="nav__link text-decoration-none">
-                            <i className="uil uil-scenery nav__icon"></i> Portfolio
-                        </Link>
-                    </li>
-                    <li className="nav__item">
-                        <Link href="#contact" className="nav__link text-decoration-none">
-                            <i className="uil uil-message nav__icon"></i>Contactme
-                        </Link>
-                    </li>
-                </ul>
-                <i className="uil uil-times nav__close nav__icon" id="nav-close"></i>
-            </div>
+  function isActive(pathname) {
+    return router.pathname === pathname;
+  }
 
-            <div className="nav__btns">
-                <div className="nav__toggle" id="nav-toggle">
-                    <i className="uil uil-apps nav__icon"></i>
-                </div>
-            </div>
-        </nav>
+  return (
+    <header className="header" id="header">
+      <nav className="nav container">
+        <div className="nav__menu" id="nav-menu">
+          <ul className="nav__list grid ">
+            <li className="nav__item">
+              <Link href="/" className={`nav__link ${isActive('/') ? 'active-link' : ''} ` }style={{ textDecoration: 'none' }}>
+                <i className="uil uil-estate nav__icon "></i>Home
+              </Link>
+            </li>
+            <li className="nav__item ">
+              <Link href="/about" className={`nav__link ${isActive('/about') ? 'active-link' : ''}`}style={{ textDecoration: 'none' }}>
+                <i className="uil uil-user nav__icon"></i>About
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link href="#skills" className={`nav__link ${isActive('/#skills') ? 'active-link' : ''}`}style={{ textDecoration: 'none' }}>
+                <i className="uil uil-file-alt nav__icon"></i>Skills
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link href="#services" className={`nav__link ${isActive('/#services') ? 'active-link' : ''}`}style={{ textDecoration: 'none' }}>
+                <i className="uil uil-briefcase-alt nav__icon"></i>Services
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link href="#portfolio" className={`nav__link ${isActive('/#portfolio') ? 'active-link' : ''}`}style={{ textDecoration: 'none' }}>
+                <i className="uil uil-scenery nav__icon"></i> Portfolio
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link href="#contact" className={`nav__link ${isActive('/#contact') ? 'active-link' : ''}`}style={{ textDecoration: 'none' }}>
+                <i className="uil uil-message nav__icon"></i>Contactme
+              </Link>
+            </li>
+          </ul>
+          <i className="uil uil-times nav__close nav__icon" id="nav-close"></i>
+        </div>
+
+        <div className="nav__btns">
+          <div className="nav__toggle" id="nav-toggle">
+            <i className="uil uil-apps nav__icon"></i>
+          </div>
+        </div>
+      </nav>
     </header>
-    );
+  );
 }
 
 export default Header;
