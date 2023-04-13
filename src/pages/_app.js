48b7ad89359d9f9232ec8181802d9ef64c61f6bc
AerrowDev <sgaeprellegera@gmail.com>
2023-04-13 22:16:25 +0800

    add css for portfolio & preloader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/globals.css'
-import Preloader from "/src/components/Preloader";
+import Preloader from "/src/Components/Preloader";
 
 export default function App({ Component, pageProps }) {
   return (
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,14 +7,9 @@ export default function Document() {
       <Head>
       <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"/>
       <link rel="stylesheet" href="/assets/css/styles.css"/>
-      <link rel="preload" href="/assets/js/preloader.js" as="script" />
       </Head>
       <body>
-      <div id="preloader">
-        <div class="preloader-logo">
-          <img src="/assets/img/favicon.png"  width= "1000" height="80" alt="pre-loader"/>
-        </div>
-      </div>
+
         <Main />
         
         <NextScript />
