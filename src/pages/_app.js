e28d89ed903e82d27735170d4d41426229a7e301
AerrowDev <sgaeprellegera@gmail.com>
2023-05-25 21:01:35 +0800

    remove  messengerchat api

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/globals.css'
 import Preloader from "/src/Components/Preloader";
-import MessengerChat from '/src/Components/MessengerChat';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Preloader />
-      <MessengerChat />
+      
       <Component {...pageProps} />
     </>
   )
