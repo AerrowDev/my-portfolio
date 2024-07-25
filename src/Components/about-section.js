5f52e2a42187e9ae308d31cb4c3dca5ee944b91e
AerrowDev <sgaeprellegera@gmail.com>
2024-07-25 14:03:54 +0800

    update resume

--- src/Components/about-section.js
+++ src/Components/about-section.js
@@ -1,56 +1,73 @@
-import Image from 'next/image';
-import styles from '@/styles/Home.module.css'
-import Link from 'next/link';
-import 'bootstrap/dist/css/bootstrap.css'
-import Imagenav from '/src/Components/logo-nav'
-import Welcome from "src/Components/welcome"
-const Aboutsection = () =>{
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import "bootstrap/dist/css/bootstrap.css";
+import Imagenav from "/src/Components/logo-nav";
+import Welcome from "src/Components/welcome";
+const Aboutsection = () => {
     return (
         <section className="home section" id="home">
+            <div className="home__container container grid">
+                <div className="home__content grid">
+                    <div className="home__social"></div>
+                    <Welcome className="home__img" />
 
-        <div className="home__container container grid">
-            <div className="home__content grid">
-                <div className="home__social">
-                   
-                    
-                </div>
-               <Welcome  className="home__img"/>
-
-                <div className="home__data">
-                <p className=" home__welcome text-muted text-uppercase "> Let&apos;s collaborate and bring our ideas to life.</p>
-                    <h1 className="home__title">Hi, I&apos;am Aaron  </h1>
-                    <h3 className="home__subtitle">Web Developer </h3>
-                    <p className="home__description">a web developer with a passion for building beautiful and functional websites that provide a great user experience.
-                     </p>
-                    <Link download href="/assets/pdf/Aaron-Prellegerav1.pdf" className="button button--flex me-4">
-                      Download Resume<i className="uil uil-download-alt button__icon"></i>
-                    </Link>
-                    <Link href="contact/" className="text-dark button--flex">
-                        Contact Me
-                    </Link>
-                    <div className="my-5">
-                      <Link  href="https://m.me/ore.wa.haruhiro" target="_blank"
-                            className="home__social-icon px-3">
-                          <i className="uil uil-facebook-messenger-alt"></i>
+                    <div className="home__data">
+                        <p className=" home__welcome text-muted text-uppercase ">
+                            {" "}
+                            Let&apos;s collaborate and bring our ideas to life.
+                        </p>
+                        <h1 className="home__title">Hi, I&apos;am Aaron </h1>
+                        <h3 className="home__subtitle">Web Developer </h3>
+                        <p className="home__description">
+                            a web developer with a passion for building
+                            beautiful and functional websites that provide a
+                            great user experience.
+                        </p>
+                        <Link
+                            download
+                            href="/assets/pdf/AaronPrellegeraResumeupdated.pdf"
+                            className="button button--flex me-4"
+                        >
+                            Download Resume
+                            <i className="uil uil-download-alt button__icon"></i>
                         </Link>
-
-                        <Link href="https://wa.me/message/HMLDVC5ST6J2G1" target="_blank" className="home__social-icon px-3">
-                        <i className="uil uil-whatsapp"></i>
+                        <Link
+                            href="contact/"
+                            className="text-dark button--flex"
+                        >
+                            Contact Me
                         </Link>
+                        <div className="my-5">
+                            <Link
+                                href="https://m.me/ore.wa.haruhiro"
+                                target="_blank"
+                                className="home__social-icon px-3"
+                            >
+                                <i className="uil uil-facebook-messenger-alt"></i>
+                            </Link>
 
-                        <Link href="https://join.skype.com/invite/q7YWQejcIEuq" target="_blank" className="home__social-icon px-3">
-                        <i className="uil uil-skype-alt"></i>
-                        </Link>
+                            <Link
+                                href="https://wa.me/message/HMLDVC5ST6J2G1"
+                                target="_blank"
+                                className="home__social-icon px-3"
+                            >
+                                <i className="uil uil-whatsapp"></i>
+                            </Link>
+
+                            <Link
+                                href="https://join.skype.com/invite/q7YWQejcIEuq"
+                                target="_blank"
+                                className="home__social-icon px-3"
+                            >
+                                <i className="uil uil-skype-alt"></i>
+                            </Link>
+                        </div>
                     </div>
                 </div>
-                
             </div>
-           
-        </div>
-
-
-    </section>
+        </section>
     );
-}
+};
 
 export default Aboutsection;
--- src/Components/about-section2.js
+++ src/Components/about-section2.js
@@ -1,56 +1,73 @@
-import Image from 'next/image';
-import styles from '@/styles/Home.module.css'
-import Link from 'next/link';
-import 'bootstrap/dist/css/bootstrap.css'
-import Imagenav from '/src/Components/logo-nav'
-import { useEffect, useRef } from 'react';
-import CountUp from 'countup.js';
-const Aboutsection2 = () =>{
-    
-
-  
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import "bootstrap/dist/css/bootstrap.css";
+import Imagenav from "/src/Components/logo-nav";
+import { useEffect, useRef } from "react";
+import CountUp from "countup.js";
+const Aboutsection2 = () => {
     return (
-      
-    <section className="about section" id="about">
-        <h2 className="section__title">About Me </h2>
-        <span className="section__subtitle">My Introduction</span>
+        <section className="about section" id="about">
+            <h2 className="section__title">About Me </h2>
+            <span className="section__subtitle">My Introduction</span>
 
-        <div className="about__container container grid">
-            <img src="assets/img/2x2 23 - final.jpg" alt="" className="about__img"/>
-            <div className="about__data">
-                <p className="about__description">
-                I am an experienced web developer and graphic designer with a passion for creating visually appealing and functional websites and designs. 
-                I have expertise in HTML, CSS, JavaScript, and the Adobe Creative Suite, and have expanded my skills to include several popular frameworks such as Laravel, Next.js, and React. 
-                I have successfully completed various projects using these frameworks and I bring projects from concept to completion with my technical and creative abilities.
-                 My goal is to deliver high-quality work that meets the needs of clients and exceeds their expectations.
-                </p>
-                <div className="about__info">
-                    <div>
-                        <span className="about__info-title" >01</span>
-                        <span className="about__info-name" >Year/s <br/> experience</span>
+            <div className="about__container container grid">
+                <img
+                    src="assets/img/2x2 23 - final.jpg"
+                    alt=""
+                    className="about__img"
+                />
+                <div className="about__data">
+                    <p className="about__description">
+                        I am an experienced web developer and graphic designer
+                        with a passion for creating visually appealing and
+                        functional websites and designs. I have expertise in
+                        HTML, CSS, JavaScript, and the Adobe Creative Suite, and
+                        have expanded my skills to include several popular
+                        frameworks such as Laravel, Next.js, and React. I have
+                        successfully completed various projects using these
+                        frameworks and I bring projects from concept to
+                        completion with my technical and creative abilities. My
+                        goal is to deliver high-quality work that meets the
+                        needs of clients and exceeds their expectations.
+                    </p>
+                    <div className="about__info">
+                        <div>
+                            <span className="about__info-title">01</span>
+                            <span className="about__info-name">
+                                Year/s <br /> experience
+                            </span>
+                        </div>
+
+                        <div>
+                            <span className="about__info-title">05</span>
+                            <span className="about__info-name">
+                                Completed <br /> Project
+                            </span>
+                        </div>
+
+                        <div>
+                            <span className="about__info-title">02</span>
+                            <span className="about__info-name">
+                                Companies <br /> worked
+                            </span>
+                        </div>
                     </div>
 
-                    <div>
-                        <span className="about__info-title" >05</span>
-                        <span className="about__info-name">Completed <br/> Project</span>
+                    <div className="about__buttons">
+                        <a
+                            download=""
+                            href="/assets/pdf/AaronPrellegeraResumeupdated.pdf"
+                            className="button button--flex"
+                        >
+                            Download Resume{" "}
+                            <i className="uil uil-download-alt button__icon"></i>
+                        </a>
                     </div>
-
-                    <div>
-                        <span className="about__info-title" >02</span>
-                        <span className="about__info-name">Companies <br/> worked</span>
-                    </div>
-                </div>
-
-                <div className="about__buttons">
-                    <a download="" href="/assets/pdf/Aaron-Prellegerav1.pdf" className="button button--flex">
-                       Download Resume <i className="uil uil-download-alt button__icon"></i>
-                    </a>
                 </div>
             </div>
-        </div>
-
-    </section>
+        </section>
     );
-}
+};
 
 export default Aboutsection2;
